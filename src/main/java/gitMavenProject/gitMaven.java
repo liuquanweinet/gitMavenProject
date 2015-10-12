51b4b819472e7c84ff97053b030489ac2fc5a97c
lairen <lairen>
2015-10-12 15:49:45 +0800

    Initial commit

---
+++ src/main/java/gitMavenProject/gitMaven.java
@@ -0,0 +1,12 @@
+package gitMavenProject;
+
+/**
+ * Created by lairen on 15/10/12.
+ */
+public class gitMaven {
+    public static void main(String[] args){
+        for (int i = 0; i < 20; i ++) {
+            System.out.println("hello world");
+        }
+    }
+}
